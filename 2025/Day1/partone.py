38c12e45b8cc21389ccd94a2a8593d12438fb684
Pavel Knespl <pafik.knespl@gmail.com>
2025-12-06 14:45:25 +0100

    refactor

--- 2025/Day1/partone.py
+++ 2025/Day1/partone.py
@@ -1,15 +1,13 @@
-def solve():
-    pos = 50
-    count = 0
-    with open('input.txt') as f:
-        for line in f:
-            d = line[0]
-            val = int(line[1:])
-            if d == 'R':
-                pos = (pos + val) % 100
-            else:
-                pos = (pos - val) % 100
-            if pos == 0:
-                count += 1
-    print(count)
-solve()
+pos = 50
+count = 0
+with open('input.txt') as f:
+    for line in f:
+        d = line[0]
+        val = int(line[1:])
+        if d == 'R':
+            pos = (pos + val) % 100
+        else:
+            pos = (pos - val) % 100
+        if pos == 0:
+            count += 1
+print(count)
--- 2025/Day5/parttwo.py
+++ 2025/Day5/parttwo.py
@@ -1,27 +1,24 @@
-def solve():
-    with open('input.txt', 'r') as f:
-        obsah = f.read()
-    text_rozsahu = obsah.split('\n\n')[0].split()
-    rozsahy = []
-    for r in text_rozsahu:
-        start, konec = r.split('-')
-        rozsahy.append((int(start), int(konec)))
-    rozsahy.sort()
-    sloucene_rozsahy = []
-    if not rozsahy:
-        print(0)
-        return
-    aktualni_start, aktualni_konec = rozsahy[0]
-    for i in range(1, len(rozsahy)):
-        dalsi_start, dalsi_konec = rozsahy[i]
-        if dalsi_start <= aktualni_konec + 1: 
-            aktualni_konec = max(aktualni_konec, dalsi_konec)
-        else:
-            sloucene_rozsahy.append((aktualni_start, aktualni_konec))
-            aktualni_start, aktualni_konec = dalsi_start, dalsi_konec
-    sloucene_rozsahy.append((aktualni_start, aktualni_konec))
-    celkem_cerstvych = 0
-    for start, konec in sloucene_rozsahy:
-        celkem_cerstvych += (konec - start + 1)
-    print(celkem_cerstvych)
-solve()
+with open('input.txt', 'r') as f:
+    obsah = f.read()
+text_rozsahu = obsah.split('\n\n')[0].split()
+rozsahy = []
+for r in text_rozsahu:
+    start, konec = r.split('-')
+    rozsahy.append((int(start), int(konec)))
+rozsahy.sort()
+sloucene_rozsahy = []
+if not rozsahy:
+    print(0)
+aktualni_start, aktualni_konec = rozsahy[0]
+for i in range(1, len(rozsahy)):
+    dalsi_start, dalsi_konec = rozsahy[i]
+    if dalsi_start <= aktualni_konec + 1: 
+        aktualni_konec = max(aktualni_konec, dalsi_konec)
+    else:
+        sloucene_rozsahy.append((aktualni_start, aktualni_konec))
+        aktualni_start, aktualni_konec = dalsi_start, dalsi_konec
+sloucene_rozsahy.append((aktualni_start, aktualni_konec))
+celkem_cerstvych = 0
+for start, konec in sloucene_rozsahy:
+    celkem_cerstvych += (konec - start + 1)
+print(celkem_cerstvych)
